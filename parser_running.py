c615ef12b9b4728b18f1ee66df93b478c734845c
Шамаева Елена Денисовна <eshamaeva@stsoft.ru>
2025-08-01 14:52:48 +0300

    Deleting extra parsers

--- parser_running.py
+++ parser_running.py
@@ -4,83 +4,6 @@ import time
 
 from data_classes import ConllEntry, Sentence
 
-class UDPipeSpacyParser:
-    def __init__(self):
-        import spacy_udpipe
-        spacy_udpipe.download("ru")
-        self.udpipe_model = spacy_udpipe.load("ru")
-
-    def parse(self, sent):
-        parsed = self.udpipe_model(sent)
-        res = []
-        for token in parsed:
-            if token.dep_ != 'ROOT':
-              parent_id = token.head.i+1
-              relation = token.dep_
-            else:
-              parent_id = 0
-              relation = 'root'
-            c = ConllEntry(str(token.i+1), form=token.text,
-                                   parent_id=str(parent_id), relation=relation)
-            res.append(c)
-        return res
-
-
-class DeepPavlovParser:
-    def __init__(self):
-        from deeppavlov import build_model
-        import nltk
-        nltk.download('punkt_tab')
-        self.model = build_model('syntax_ru_syntagrus_bert', install=True, download=True)
-
-    def parse(self, sent):
-        conll_res = self.model([sent])
-        res = []
-        for word_res in conll_res[0].split('\n'):
-            if word_res.strip(): # ignore empty lines
-                wr = word_res.split('\t')
-                t =  ConllEntry(str(wr[0]), form=wr[1], parent_id=str(wr[6]), relation=wr[7])
-                res.append(t)
-        return res
-
-
-class SpacyParser:
-    def __init__(self):
-        import spacy
-        self.nlp = spacy.load("ru_core_news_lg")
-
-    def parse(self, sent):
-        parsing_res = self.nlp(sent)
-        res = []
-        for token in parsing_res:
-          if token.dep_ == "ROOT":
-            parent_id = 0
-            relation = "root"
-          else:
-            parent_id = token.head.i + 1
-            relation = token.dep_
-          t =  ConllEntry(str(token.i + 1), form=token.text, parent_id=str(parent_id), relation=relation)
-          res.append(t)
-        return res
-
-
-class StanzaParser:
-    def __init__(self):
-        import stanza
-        self.nlp = stanza.Pipeline(lang='ru', processors='tokenize, pos, lemma, depparse')
-
-    def parse(self, sent):
-        doc = self.nlp(sent)
-        res = []
-        for sentence in doc.sentences:
-            for word in sentence.words:
-                relation = word.deprel
-                if relation == "ROOT":
-                  relation = "root"
-                c = ConllEntry(str(word.id), form=word.text, parent_id=str(word.head), relation=relation)
-                res.append(c)
-        return res
-
 
 class NatashaParser:
     def __init__(self):
@@ -103,8 +26,7 @@ class NatashaParser:
 
 parser = argparse.ArgumentParser(description='Info about parser tests')
 parser.add_argument('pickle_data_path', type=str, help='Path')
-parser.add_argument('parser_name', choices=["stanza", "spacy", "deeppavlov",\
-                                            "udpipe", "natasha"])
+parser.add_argument('parser_name', choices=["natasha"])
 args = parser.parse_args()
 print(args.pickle_data_path)
 print(args.parser_name)
@@ -112,9 +34,7 @@ print(args.parser_name)
 with open(args.pickle_data_path, 'rb') as f:
     data = pickle.load(f)
 
-parser_dict = {'udpipe': UDPipeSpacyParser, 'stanza': StanzaParser,\
-               'natasha': NatashaParser, 'deeppavlov': DeepPavlovParser,\
-               'spacy': SpacyParser}
+parser_dict = { 'natasha': NatashaParser }
 parser_class = parser_dict[args.parser_name]
 parser = parser_class()
 
